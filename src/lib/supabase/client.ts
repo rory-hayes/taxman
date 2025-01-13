0d9c8a5bfa3488a71557c48285b70e6d79fe2fc3
rory-hayes <54278439+rory-hayes@users.noreply.github.com>
2025-01-13 19:40:52 +0000

    build error

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,16 +2,10 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/lib/types/supabase'
 
 export const createClient = () => {
-  const supabase = createClientComponentClient<Database>({
-    auth: {
-      flowType: 'pkce',
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-      persistSession: true,
-      redirectTo: process.env.NEXT_PUBLIC_SUPABASE_SITE_URL 
-        ? `${process.env.NEXT_PUBLIC_SUPABASE_SITE_URL}/auth/callback`
-        : 'http://localhost:3000/auth/callback'
+  return createClientComponentClient<Database>({
+    cookieOptions: {
+      name: "sb-auth-token",
+      path: "/",
     }
   })
-  return supabase
 } 
